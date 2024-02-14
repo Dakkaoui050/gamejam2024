e8533207859b980373317d8442e09b29f94b210d
peppie9728 <56298154+peppie9728@users.noreply.github.com>
2024-02-14 13:46:00 +0100

    jo

---
+++ test/Assets/Block.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public bool inTower;
+    public bool firstBlock;
+    public buildZone bz;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "block")
+        {
+            Block temp = collision.gameObject.GetComponent<Block>();
+            if (temp.inTower)
+            {
+               bz = GetComponent<buildZone>();
+               bz.AddBlock(gameObject);
+               inTower = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "buildZone")
+        {
+            bz = other.gameObject.GetComponent<buildZone>();
+            inTower = true;
+        }
+    }
+}
